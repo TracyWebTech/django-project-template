b1d4054c2ccb8ca6eeb8cfbc39eb4111fe5e826f
Luan Pablo <luanpab@gmail.com>
2014-04-10 10:22:11 -0300

    Adding appdeploy django to puppet init

--- puppet/modules/project_name/manifests/init.pp
+++ puppet/modules/project_name/manifests/init.pp
@@ -1,4 +1,11 @@
 class {{ project_name }} {
 
+  appdeploy::django { '{{ project_name }}':
+    user      => '{{ project_name }}',
+    directory => '/home/{{ project_name }}/{{ project_name }}/site',
+    proxy_hosts => [
+      '{{ project_name }}.placeholder',
+    ],
 
+  }
 }
