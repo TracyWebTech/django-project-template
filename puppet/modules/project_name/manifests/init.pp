6a0de584a48bb956f2b43868c971b9676ec721fb
Luan Pablo <luanpab@gmail.com>
2014-04-10 10:58:33 -0300

    Minor fix

--- puppet/modules/project_name/manifests/init.pp
+++ puppet/modules/project_name/manifests/init.pp
@@ -2,7 +2,7 @@ class {{ project_name }} {
 
   appdeploy::django { '{{ project_name }}':
     user      => '{{ project_name }}',
-    directory => '/home/{{ project_name }}/{{ project_name }}/site',
+    directory => '/home/{{ project_name }}/{{ project_name }}/src',
     proxy_hosts => [
       '{{ project_name }}.placeholder',
     ],
