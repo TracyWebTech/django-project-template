b7809a86d7474937cdd55654a049f4e58bc5cf47
Sergio Oliveira <sergio@tracy.com.br>
2014-07-28 19:20:12 -0300

    Sample project configuration

---
+++ project_cfg.py
@@ -0,0 +1,17 @@
+
+repository_url =  'TO BE DEFINED'
+
+environments = {
+    'qa': {
+      'hosts': [],
+      'port': 22,
+      'is_vagrant': False,
+      'superuser': 'root',
+    },
+    'prod': {
+        'hosts': [],
+        'port': 22,
+        'is_vagrant': False,
+        'superuser': 'root',
+    },
+}
