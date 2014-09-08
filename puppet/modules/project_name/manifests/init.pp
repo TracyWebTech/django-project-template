9970e7d6a2408d765616fe9e0cba32850a16da10
Sergio Oliveira <sergio@tracy.com.br>
2014-09-08 15:08:19 -0300

    No need to set directory since the default works fine

--- puppet/modules/project_name/manifests/init.pp
+++ puppet/modules/project_name/manifests/init.pp
@@ -2,7 +2,6 @@ class {{ project_name }} {
 
   appdeploy::django { '{{ project_name }}':
     user      => '{{ project_name }}',
-    directory => '/home/{{ project_name }}/{{ project_name }}/src',
     proxy_hosts => [
       '{{ project_name }}.placeholder',
     ],
